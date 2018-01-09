d86b405e9934cf6a4dc67d4ff0ba358edc5d4374
Gunnar Arndt <gunnar.arndt@vacos.de>
2018-01-09 17:40:27 +0100

    NetBeans project configuration.

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=SysGCC-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/SysGCC-Windows
+CND_ARTIFACT_NAME_Debug=libunits.a
+CND_ARTIFACT_PATH_Debug=dist/Debug/SysGCC-Windows/libunits.a
+CND_PACKAGE_DIR_Debug=dist/Debug/SysGCC-Windows/package
+CND_PACKAGE_NAME_Debug=units.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/SysGCC-Windows/package/units.tar
+# Release configuration
+CND_PLATFORM_Release=SysGCC-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/SysGCC-Windows
+CND_ARTIFACT_NAME_Release=libunits.a
+CND_ARTIFACT_PATH_Release=dist/Release/SysGCC-Windows/libunits.a
+CND_PACKAGE_DIR_Release=dist/Release/SysGCC-Windows/package
+CND_PACKAGE_NAME_Release=units.tar
+CND_PACKAGE_PATH_Release=dist/Release/SysGCC-Windows/package/units.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
